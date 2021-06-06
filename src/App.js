26a95c6c7a64f81064c5018519bac677b1723796
Lor3-dev <lor.develop@gmail.com>
2021-06-06 11:13:02 +0200

    Search engine

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header"></header>
-      Hello
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer">
+        <small>This project was coded by Lorena Ibeas and is <a href="#" target="_blank">open-sourced on GitHub</a></small>
+      </footer>
+      </div>
     </div>
   );
 }
