6ffd66cd50687b5ea159b1660cd6e28783fcec8a
Lor3-dev <lor.develop@gmail.com>
2021-06-06 13:28:31 +0200

    Added API documentation URL

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -11,6 +11,7 @@ export default function Dictionary() {
 
     function search(event) {
         event.preventDefault();
+        //Documentation: https://dictionaryapi.dev
         let apiUrl=`https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
         axios.get(apiUrl).then(handleResponse);
     }
